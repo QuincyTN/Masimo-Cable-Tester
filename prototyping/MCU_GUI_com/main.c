ce25ff842d5ca2ef08ebe055e742408ffb7f1c71
Quincy Nguyen <qnguyen5882@sdsu.edu>
2024-11-14 20:23:22 -0800

    Attempt send data from HMI to MCU
    It does not work. The receive buffer does not fill up? Tried to implement receiver without the interrupt using receiver3() which reads 1 char. After pressing the HMI button, it does not read.

--- prototyping/MCU_GUI_com/main.c
+++ prototyping/MCU_GUI_com/main.c
@@ -7,7 +7,6 @@
 
 #define F_CPU 24000000UL
 #include <avr/io.h>
-//#include <avr/ioavr128db48.h>
 #include <inttypes.h>
 #include <avr/interrupt.h>
 #include <avr/cpufunc.h>
@@ -17,8 +16,14 @@
 
 #define BAUDr 9600 
 
-volatile char transmitData[];
-volatile uint8_t dataIndex = 0;
+volatile char transmitData[50];
+volatile uint8_t dataIndexTx = 0;
+volatile char receiveData[4];
+volatile uint8_t dataIndexRx = 0;
+
+volatile uint8_t enable = 0;
+
+volatile char test[1];
 
 void port_init(){
 	/*
@@ -27,6 +32,7 @@ void port_init(){
 	x can be A,B,C,D,E,F with register sizes PA[7:0], PB[5:0], PC[7:0], PD[7:0], PE[3:0], PF[6:0]
 	*/
 	PORTB.DIRSET = (1<<3);		//set 3rd pin of PORTB to output, LED on board
+	PORTB.OUT |= (1<<3);		//turn LED off
 }
 
 
@@ -40,7 +46,7 @@ void initUSART3(){
 	//uint16_t BAUDRATE = F_CPU / (2UL * BAUDr);	//synchronous baudrate calc
 	PORTMUX.USARTROUTEA &= 0;	//USART3 using PB0 as TxD and PB1 as RxD
 	PORTB.DIRSET = (1 << 0);    //Set PB0 as output (TxD)
-	PORTB.DIRCLR = (1 << 1);	//Set PB1 as input (RxD)
+	//PORTB.DIRCLR = (1 << 1);	//Set PB1 as input (RxD)
 
 	//not sure if I set the baud rate correctly
 	USART3.BAUD = BAUDRATE;
@@ -50,20 +56,21 @@ void initUSART3(){
 	//USART3.CTRLC |= (1<<6);		//USART is synchronous, no parity bit, 1 stop bit, 8 char data size
 	USART3.CTRLB |= (1<<7) | (1<<6);	//enable receiver and transmitter
 	
-	//USART3.CTRLA |= (1<<7) | (1<<6) | (1<<5);	//enable receive complete interrupt and transmit complete interrupt, and data register empty interrupt
+	//USART3.CTRLA = (USART_RXCIE_bm);	//enable receive complete interrupt and transmit complete interrupt, and data register empty interrupt
+
 }
-
-
 
 void transmit3(char* ID, char* str){
 	
 	sprintf(transmitData, "%s.txt=\"%s\"%c%c%c", ID, str, 0xFF, 0xFF, 0xFF);
-	USART3.CTRLA = (1 << USART_DREIE_bp);	//turn on the data empty register
+	USART3.CTRLA = (1 << USART_DREIE_bp);	//turn on the data empty register interrupt
+	
+	
 	/*
 	char command[50];
 	int len = sprintf(command, "%s.txt=\"%s\"", ID, str);
 	
-	//PORTB.OUT |= (1<<3);	//turn LED on
+	//PORTB.OUT |= (1<<3);	//turn LED off
 	//_delay_ms(500);
 			
 	for(int i = 0; i < len; i++){
@@ -79,7 +86,7 @@ void transmit3(char* ID, char* str){
 	//while(!(USART3.STATUS & (1<<5)));	//wait until all data in buffer is sent
 	//USART3.TXDATAL = "\n";
 	
-	//PORTB.OUT &= ~(1<<3);	//turn LED off
+	//PORTB.OUT &= ~(1<<3);	//turn LED on
 	//_delay_ms(500);
 }
 
@@ -88,37 +95,68 @@ ISR(USART3_DRE_vect) {
 	/*
 	Send data to the HMI via USART3
 	The interrupt is enable only after the transmit3() function
-	*/
-	if (transmitData[dataIndex] != NULL) {         //Check if more data to send
-		USART3.TXDATAL = transmitData[dataIndex];  //Send next character
-		dataIndex++;                               //Move to next character
+	*/	
+	if (transmitData[dataIndexTx] != NULL) {         //Check if more data to send
+		USART3.TXDATAL = transmitData[dataIndexTx];  //Send next character
+		dataIndexTx++;                               //Move to next character
 	} 
 	else {
-		USART3.CTRLA &= ~(1 << USART_DREIE_bp);    //Disable DRE interrupt when done
-		dataIndex = 0;                             //Reset index for next transmission
+		USART3.CTRLA &= ~(1 << USART_DREIE_bp);      //Disable DRE interrupt when done
+		dataIndexTx = 0;                             //Reset index for next transmission
 	}
+}
+
+ISR(USART3_RXC_vect){
+	/*
+	Interrupt occurs when HMI receives data (a command or action)
+	*/
+	
+	/*
+	test[0] = USART3.RXDATAL;
+	transmit3("hi", test);
+	*/
+	
+	receiveData[dataIndexRx] = USART3.RXDATAL;
+	
+	dataIndexRx++;
+	if(dataIndexRx >= 4){
+		dataIndexRx = 0;
+		
+		if(receiveData[0] == '1')	//check first byte of message
+			enable = 1;
+		else
+			enable = 0;
+	}
+}
+
+char receiver3(){
+	while(!(USART3.STATUS & USART_RXCIF_bm));
+	char a = USART3.RXDATAL;
+	return a;
 }
 
 int main(void)
 {
 	ccp_write_io(&(CLKCTRL.OSCHFCTRLA),CLKCTRL_FRQSEL_24M_gc);	//set main clock to 24MHz
-	sei();	//turn on global interrupts
 	port_init();	//initialize ports
 	initUSART3();	//initialize USART3
-	
+	sei();	//turn on global interrupts
 	char data[50];
 	
 	uint8_t counter = 0;
+	char receivedByte = '1';
+	enable = 1;
     while (1) 
     {
-		while(counter<15){
+		if(enable){
 			sprintf(data, "%d", counter);
-		
+			
 			transmit3("t0", data);
-		
-			counter = (counter+1);
+			//transmit3("hi", &receivedByte);
+			counter = (counter+1)%100;
 			_delay_ms(1000);
 		}
+		//receivedByte = receiver3();
 
     }
 }

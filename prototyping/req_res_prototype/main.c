2b50a70f69d4c05332307daa1acb9fd37d11fba1
Quincy Nguyen <qnguyen5882@sdsu.edu>
2025-02-10 21:05:31 -0800

    Debug Terminal Setup
    A Putty terminal can be used to take an input and do something on the MCU. Added parseTerminalData() and parseHmiData() functions to do an action after a new terminal or HMI message is received

--- prototyping/req_res_prototype/main.c
+++ prototyping/req_res_prototype/main.c
@@ -40,6 +40,10 @@ The attribute is in the form of "[page].[ID].[field]" (eg "mcu.n0.val")
 #define BUFFER_SIZE 50
 #define PRESCALER 1024
 
+//HMI starting return message
+#define STRING_MESSAGE 0x70		//message has a string
+#define NUM_MESSAGE 0x71		//message is an integer 
+
 
 //USART2 transmitter and receiver buffer 
 char transmitData[BUFFER_SIZE];
@@ -47,22 +51,27 @@ uint8_t dataIndexTx = 0;
 char receiveData[BUFFER_SIZE];
 uint8_t dataIndexRx = 0;
 
-uint8_t receiving = 0;		//flag for strings
-uint8_t messageType = 0;	//0x71 for strings, 0x70 for ints
+uint8_t hmiReceiving = 0;			//flag for HMI
+uint8_t terminalReceiving = 0;		//flag for terminal
 
 //Flags for USART 
 uint8_t enable = 0;
-uint8_t sendMessage = 0;
+uint8_t newHmiMessage = 0;
+uint8_t newTerminalMessage = 0;
 uint8_t receiveFlag = 0;
-uint8_t timer_flag = 0;
+uint8_t timerFlag = 0;
 
 //USART3 transmitter and receiver buffer 
 char transmitData3[BUFFER_SIZE];
 uint8_t dataIndexTx3 = 0;
-char receiveData3[4];
+char receiveData3[BUFFER_SIZE];
 uint8_t dataIndexRx3 = 0;
 
-void port_init(){
+//Buffers for the receiver of the terminal and HMI
+char terminalBuffer[BUFFER_SIZE];
+char hmiBuffer[BUFFER_SIZE];
+
+void portInit(){
 	/*
 	Purpose: initialize port and pin directions (as input/output)
 	PORTx.DIRSET is the register to set port x's pin directions
@@ -83,23 +92,19 @@ void initUSART3(){
 	USART3.BAUD = BAUDRATE;
 	
 	//USART3.CTRLC |= (1<<6);		//USART is synchronous, no parity bit, 1 stop bit, 8 char data size
-	//USART3.CTRLB = (USART_RXEN_bm);	//enable receiver
-	USART3.CTRLB = (USART_TXEN_bm); //enable transmitter
+	USART3.CTRLB = (USART_RXEN_bm) | (USART_TXEN_bm); //enable receiver and transmitter
 	
-	USART3.CTRLA = (USART_RXCIE_bm);	//enable receive complete interrupt and transmit complete interrupt, and data register empty interrupt
+	USART3.CTRLA = (USART_RXCIE_bm);	//enable receive complete interrupt
 }
 
-void transmit3(char* ID, char* str){
-	char command[50];
-	int len = sprintf(command, "%s.txt=\"%s\"%c%c%c\n", ID, str, 0xFF, 0xFF, 0xFF);
-			
-	for(int i = 0; i < len; i++){
+void transmitTerminal(char* str){
+	for(int i = 0; i < strlen(str); i++){
 		while(!(USART3.STATUS & (1<<5)));	//wait until all data in buffer is sent
-		USART3.TXDATAL = (char)command[i];	//send new char
+		USART3.TXDATAL = (char)str[i];		//send new char
 	}
 	
-	//while(!(USART3.STATUS & (1<<5)));		//wait until all data in buffer is sent
-	//USART3.TXDATAL = "\n";
+	while(!(USART3.STATUS & (1<<5)));		//wait until all data in buffer is sent
+	USART3.TXDATAL = '\f';
 }
 
 void initUSART2(){
@@ -114,10 +119,10 @@ void initUSART2(){
 
 	USART2.CTRLB = (USART_RXEN_bm) | (USART_TXEN_bm);	//enable receiver and transmitter
 
-	USART2.CTRLA = (USART_RXCIE_bm);	//enable receive complete interrupt and transmit complete interrupt, and data register empty interrupt
+	USART2.CTRLA = (USART_RXCIE_bm);	//enable receive complete interrupt
 }
 
-void transmit2(char* page, char* ID, char* field, char* value, uint8_t request){
+void transmitHmi(char* page, char* ID, char* field, char* value, uint8_t request){
 	/*
 	This function will send commands to the HMI. page
 	*/
@@ -137,7 +142,6 @@ void transmit2(char* page, char* ID, char* field, char* value, uint8_t request){
 		
 }
 
-
 void initTimer1s() {
 	//Set the period for 1 second
 	TCA0.SINGLE.PER = (F_CPU / PRESCALER) - 1; // 23437 for 1s
@@ -151,112 +155,138 @@ void initTimer1s() {
 
 ISR(TCA0_OVF_vect) {
 	//get a value every second
-	switch((timer_flag++)%4){
-		case 0: transmit2(PAGE_MCU,N0,VAL,NULL, 1); break;
-		case 1: transmit2(PAGE_MCU,N1,VAL,NULL, 1); break;
-		case 2: transmit2(PAGE_MCU,T2,TXT,NULL, 1); break;
-		case 3: transmit2(PAGE_MCU,T3,TXT,NULL, 1); break;
-		default: transmit2(PAGE_MCU,N0,VAL,NULL, 1); break;
+	switch((timerFlag++)%4){
+		case 0: transmitHmi(PAGE_MCU,N0,VAL,NULL, 1); break;
+		case 1: transmitHmi(PAGE_MCU,N1,VAL,NULL, 1); break;
+		case 2: transmitHmi(PAGE_MCU,T2,TXT,NULL, 1); break;
+		case 3: transmitHmi(PAGE_MCU,T3,TXT,NULL, 1); break;
+		default: transmitHmi(PAGE_MCU,N0,VAL,NULL, 1); break;
 	}
 		
 	TCA0.SINGLE.INTFLAGS = TCA_SINGLE_OVF_bm;	//reset overflow flag
 }
 
 ISR(USART3_RXC_vect){
-	receiveData3[dataIndexRx3] = USART3.RXDATAL;
-	dataIndexRx3++;
-	
-	if(dataIndexRx3 >= 4){
-		dataIndexRx3 = 0;
-		
-		
-		if(receiveData3[0] == '1')	//check first byte of message
-			enable = 1;
-		else
-			enable = 0;
-		
-		PORTB.OUTTGL = 1<<3;
-		//transmit3("test", receiveData3);
-		//memset(receiveData3, 0, sizeof(receiveData3));
+	uint8_t data = USART3.RXDATAL;
+
+	if ((data != NULL) && !terminalReceiving) {
+		dataIndexRx3 = 0;   //Reset index
+		terminalReceiving = 1;      //Start recording message
+		memset(receiveData3, 0, sizeof(receiveData3));	//clear buffer before new message
 	}
+
+	if (terminalReceiving) {
+		receiveData3[dataIndexRx3++] = data;
+		if (data == '\r') {
+			receiveData3[dataIndexRx3-1] = '\0';
+			terminalReceiving = 0;  //Stop receiving
+			dataIndexRx3 = 0;  //Reset buffer index
+			newTerminalMessage = 1;
+			//PORTB.OUTTGL = 1<<3;	//toggle LED
+		}
 	
+		//Prevent buffer overflow
+		if (dataIndexRx3 >= BUFFER_SIZE) {
+			dataIndexRx3 = 0;
+			terminalReceiving = 0;
+		}
+	}
 	
 }
 
 ISR(USART2_RXC_vect){
 	/*
-	Interrupt occurs when HMI receives data (a command or action)
+	Interrupt occurs when HMI receives data (a command or action) from USART2 receiver
 	*/
 	
 	uint8_t data = USART2.RXDATAL;
 
 	//Check for start identifiers (0x70 or 0x71)
-	if ((data == 0x70 || data == 0x71) && !receiving) {
+	if ((data != NULL) && !hmiReceiving) {
 		dataIndexRx = 0;   //Reset index
-		receiving = 1;      //Start recording message
-		messageType = data; //Store message type
+		hmiReceiving = 1;      //Start recording message
 		memset(receiveData, 0, sizeof(receiveData));	//clear buffer before new message
 	}
 
-	if (receiving) {
+	if (hmiReceiving) {
 		receiveData[dataIndexRx++] = data;
 		if (dataIndexRx >= 3 &&
 		receiveData[dataIndexRx - 1] == 0xFF &&
 		receiveData[dataIndexRx - 2] == 0xFF &&
 		receiveData[dataIndexRx - 3] == 0xFF) {
 			
-			receiving = 0;  //Stop receiving
+			hmiReceiving = 0;  //Stop receiving
 			dataIndexRx = 0;  //Reset buffer index
-			sendMessage = 1;	//transmit data received to terminal
+			newHmiMessage = 1;
 			//PORTB.OUTTGL = 1<<3;	//toggle LED
 		}
 		
 		//Prevent buffer overflow
 		if (dataIndexRx >= BUFFER_SIZE) {
 			dataIndexRx = 0;
-			receiving = 0;
+			hmiReceiving = 0;
 		}
 	}
 }
 
-char* parseUsartStr(char* strData){
-	uint8_t newLen = strlen(strData) - 4;	//length of string does not include first or last 3 chars
-	char* parsedData = (char*)malloc(newLen + 1);	//allocate space for string
-	
-	for(int i = 1; i < newLen+1; i++)
-		parsedData[i-1] = strData[i];	//extract string between 1 and ending 0xFF
-	
-	parsedData[newLen] = '\0';	//terminate string
-	
-	return parsedData;
+void parseHmiData(char* strData){
+	memset(hmiBuffer, 0, sizeof(hmiBuffer));	//clear the hmiBuffer
+	uint8_t dataLength = strlen(strData);		//get length of data
+	if(strData[0] == STRING_MESSAGE){
+		for(int i = 1; i <= dataLength-4; i++)
+			hmiBuffer[i-1] = strData[i];	//extract string between 1 and ending 0xFF
+	}
+	else if(strData[0] == NUM_MESSAGE){
+		uint32_t intValue = ((uint32_t)strData[1]) + ((uint32_t)strData[2]*256) + ((uint32_t)strData[3]*65536) + ((uint32_t)strData[4]*16777216);
+		sprintf(hmiBuffer, "%lu", intValue);	//convert the integer to a string of characters
+	}
 }
 
-uint32_t convertUsartInt(char* strData){
-	uint32_t intValue = ((uint32_t)strData[1]) + ((uint32_t)strData[2]*256) + ((uint32_t)strData[3]*65536) + ((uint32_t)strData[4]*16777216);
-	return intValue;
+void parseTerminalData(char* strData){
+	memset(terminalBuffer, 0, sizeof(terminalBuffer));	//clear the terminalBuffer
+	strcpy(terminalBuffer, strData);	//copy data to new buffer
+	
+	if(strcmp(terminalBuffer, "led on") == 0) {
+		PORTB.OUTCLR = PIN3_bm;	//turn on led
+	}
+	else if(strcmp(terminalBuffer, "led off") == 0) {
+		PORTB.OUTSET = PIN3_bm;	//turn off led
+	}
 }
 
 int main(void)
 {
     ccp_write_io(&(CLKCTRL.OSCHFCTRLA),CLKCTRL_FRQSEL_24M_gc);	//set main clock to 24MHz
-	port_init();
+	portInit();
     initUSART3();	//initialize USART3 to display information received from HMI
     initUSART2();	//initialize USART2 to transmit and receive from HMI
-	initTimer1s();	//initialize 1s timer
+	//initTimer1s();	//initialize 1s timer
     sei();			//turn on global interrupts
-    
+	
     while (1) 
     {
-		//Display the message on the terminal using USART 3
-		if(sendMessage){
-			if(messageType == 0x70)
-				transmit3("new", parseUsartStr(receiveData));
-			else{
-				char send[BUFFER_SIZE];
-				sprintf(send, "%lu", convertUsartInt(receiveData));
-				transmit3("new", send);
-			}
-			sendMessage = 0;
+		//Display the message on the terminal using USART3
+		if(newHmiMessage){
+			USART2.CTRLB &= ~USART_RXEN_bm;	//turn off the receiver
+			USART2.CTRLA &= ~USART_RXCIE_bm;	//turn off the receive interrupt
+
+			parseHmiData(receiveData);	//parse data and store in hmiBuffer
+			transmitTerminal(hmiBuffer);	//display data on the terminal
+			newHmiMessage = 0;	//reset flag
+
+			USART2.CTRLB = USART_RXEN_bm;	//turn off the receiver
+			USART2.CTRLA = USART_RXCIE_bm;	//turn off the receive interrupt
+		}
+
+		if(newTerminalMessage){
+			USART3.CTRLB &= ~USART_RXEN_bm;	//turn off the receiver
+			USART3.CTRLA &= ~USART_RXCIE_bm;	//turn off the receive interrupt
+			
+			parseTerminalData(receiveData3);	//parse data
+			newTerminalMessage = 0;	//reset flag
+
+			USART3.CTRLB = USART_RXEN_bm;	//turn off the receiver
+			USART3.CTRLA = USART_RXCIE_bm;	//turn off the receive interrupt
 		}
     }
 }
